5aa307f374814dec9fc9c5d5a77ca2667df8d36e
JonasButikofer <brojobutikofer@gmail.com>
2025-03-15 19:18:10 -0600

    Add project files.

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _waterContext;
+        public WaterController(WaterDbContext temp) { 
+
+            _waterContext = temp;
+        }
+
+        public Ie
+    }
+}
